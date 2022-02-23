e8b42e473f96f04c43122a80ef4ae48c2200779f
franklin <f.j.a.8421@gmail.com>
2022-02-23 15:27:16 -0400

    change_3

--- contracts/Token721.sol
+++ contracts/Token721.sol
@@ -1,41 +1,111 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract Token721 is ERC20, ERC20Burnable, Pausable, AccessControl {
+contract Token721 is ERC721, ERC721Enumerable, ERC721URIStorage, Pausable, AccessControl, ERC721Burnable {
+    using Counters for Counters.Counter;
+
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    Counters.Counter private _tokenIdCounter;
+    mapping(uint256 => string) private _hashIPFS;
 
-    constructor() ERC20("Token721", "FJA") {
+    constructor() ERC721("Token721", "FJA") {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(PAUSER_ROLE, msg.sender);
         _grantRole(MINTER_ROLE, msg.sender);
     }
 
 
-/*
+    //      mint
+
+    function mint(address _to, string[] memory _hashes) public onlyRole(MINTER_ROLE){
+
+        uint256 supply = totalSupply();
+
+        for (uint256 i = 0; i < _hashes.length; i++){
+            _safeMint(_to, supply + 1);
+            _hashIPFS[supply + 1] = _hashes[i];
+        }
+
+
+    }
+
+
+    //
+
+
+
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://ipfs.io/ipfs/";
+    }
+
+    
     function pause() public onlyRole(PAUSER_ROLE) {
         _pause();
     }
+    
 
+    
     function unpause() public onlyRole(PAUSER_ROLE) {
         _unpause();
     }
+    
 
-    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
-        _mint(to, amount);
+    
+    function safeMint(address to, string memory uri) public onlyRole(MINTER_ROLE) {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
     }
+    
 
-    function _beforeTokenTransfer(address from, address to, uint256 amount)
+    
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
         internal
         whenNotPaused
-        override
+        override(ERC721, ERC721Enumerable)
     {
-        super._beforeTokenTransfer(from, to, amount);
+        super._beforeTokenTransfer(from, to, tokenId);
     }
-    */
+    
+
+    // The following functions are overrides required by Solidity.
+
+    
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+    
+
+    
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+    
+
+    
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable, AccessControl)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+    
 }
